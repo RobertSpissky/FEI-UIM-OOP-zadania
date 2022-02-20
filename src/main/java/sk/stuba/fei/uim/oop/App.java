230eab364203a008e2a7a80577fd59f7ec2ec88d
RobertSpissky <xspissky@stuba.sk>
2022-02-20 22:12:41 +0100

    switch

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,21 +2,18 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int celeCislo =5;
-        long celeVacsieCislo = 5L;
-        float desatinneCislo =5.5f;
-        double dlhsieDesatinneCislo = 5.5;
-        char znak = 'a';
-        String retazec = "retazec";
-        boolean bool = false;
-
-        int podiel =2/3; //0
-        System.out.println(podiel);
-
-        int zvysok =2%3; //2
-        System.out.println(zvysok);
-
-        float podielFloat =2/3.0f; //0
-        System.out.println(podielFloat);
+        for(int cislo = 1;cislo<4;cislo++)
+        {
+            switch(cislo) {
+                case 1:
+                    System.out.println("cislo je jedna");
+                    break;
+                case 2:
+                    System.out.println("cislo je dva");
+                    break;
+                default:
+                    System.out.println("cislo je ine ako jedna a dva");
+            }
+        }
     }
 }
